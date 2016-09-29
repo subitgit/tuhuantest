9697aeb69dc485bba86b7d6e25d99fa3c36e6136
Zhang_suzaku <suzaku_bitex@163.com>
2016-09-29 08:51:30 +0800

    test

--- app/src/main/java/com/tuhuanjk/tuhuantest/DemoActivity.java
+++ app/src/main/java/com/tuhuanjk/tuhuantest/DemoActivity.java
@@ -1,13 +1,44 @@
 package com.tuhuanjk.tuhuantest;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.res.TypedArray;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class DemoActivity extends AppCompatActivity {
+public class DemoActivity extends Activity {
+    private TextView tv_dia;
+    protected int activityCloseEnterAnimation;
 
+    protected int activityCloseExitAnimation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo);
+        tv_dia = (TextView) findViewById(R.id.tv_dia);
+
+        TypedArray activityStyle = getTheme().obtainStyledAttributes(new int[] {android.R.attr.windowAnimationStyle});
+
+        int windowAnimationStyleResId = activityStyle.getResourceId(0, 0);
+
+        activityStyle.recycle();
+
+        activityStyle = getTheme().obtainStyledAttributes(windowAnimationStyleResId, new int[] {android.R.attr.activityCloseEnterAnimation, android.R.attr.activityCloseExitAnimation});
+
+        activityCloseEnterAnimation = activityStyle.getResourceId(0, 0);
+
+        activityCloseExitAnimation = activityStyle.getResourceId(1, 0);
+
+        activityStyle.recycle();
+
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(activityCloseEnterAnimation,activityCloseExitAnimation);
     }
 }
--- app/src/main/java/com/tuhuanjk/tuhuantest/MainActivity.java
+++ app/src/main/java/com/tuhuanjk/tuhuantest/MainActivity.java
@@ -1,13 +1,25 @@
 package com.tuhuanjk.tuhuantest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btn_dia;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Intent intent = new Intent(this,DemoActivity.class);
+        btn_dia = (Button) findViewById(R.id.btn_dia);
+        btn_dia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
+
     }
 }
